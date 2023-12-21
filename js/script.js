7de30d83a66bf68e5838d4e17a2c46d9ca17535e
tako1995 <141060940+tako1995@users.noreply.github.com>
2023-12-21 19:05:13 +0400

    edit

--- js/script.js
+++ js/script.js
@@ -151,9 +151,9 @@ function createDots() {
   arrowLeft.addEventListener("click", arrowLeftClick);
   arrowRight.addEventListener("click", arrowRigthClick);
 
-  // setInterval(() => {
-  //  arrowRigthClick();
-  //   }, 4000);
+  setInterval(() => {
+   arrowRigthClick();
+    }, 3000);
 
 
 
@@ -163,16 +163,12 @@ function createDots() {
 
 let formElement = document.getElementById("resgitration");
 
-//submit - გვერდი რომ არ დარეფრეშდეს
+
 
 formElement.addEventListener("submit", function (event) {
   event.preventDefault();
 
-  let errors = {}; // შეცდომის ობიექტი, თუ მომხმარებელს არ ექნება შეცდუმა ერორ ობიექტი ცაარიელი იქნება
-
-  //username
-  // usernameValue - ცვლადი
-  // usernameValue - მომხმარებლის ჩაწერილი ინფო იქნება ჩაწერილი ამ ცვლადში
+  let errors = {}; 
 
   let usernameValue = document.getElementById("usernamefield").value;
 
@@ -184,9 +180,7 @@ formElement.addEventListener("submit", function (event) {
   let passwValue = document.getElementById("passwordfield").value;
   let passw2Value = document.getElementById("passwordfield2").value;
 
-  // თუ მომხმარებელს ამ ველში არაფერი ექნება ჩაწერილი ამოაგდებს Password field can not be empty
-
-  // თუ პირველი პაროლი არ ემთხვევა მეორე პაროლს მაშინ ამოვარდება შეცდომა და ამოვარდება Passwords do not match
+ 
 
   if (passwValue == "") {
     errors.passw = "Password field can not be empty";
@@ -196,13 +190,7 @@ formElement.addEventListener("submit", function (event) {
     errors.passw2 = "Passwords do not match";
   }
 
-  // radio
-
-  // radio
-
-  //gender - ცვლადი
-
-// რომელიმე ერთი ანუ ქალია თუ კაცია არის თუ არა მონიშნული ამით უნდა გადავუაროთ foreach-ს საშუალებით
+ 
 
   let gender = false;
 
@@ -214,17 +202,12 @@ formElement.addEventListener("submit", function (event) {
     }
   });
 
-// ! შემობრუნებული ვარიანტი, საპირისპირო ჩანაწერი
-// თუ მომხმარებელმა არაფერი მონიშნა გამოჩნდება ერორი და დაეწერება Please select Your Gender
+
 
   if (!gender) {
     errors.gender = "Please select Your Gender";
   }
 
-  //checkbox
-
-  // ამ დროს გვაინტერესებს მონიშნულია თუ არა როგორც სქესის დროს და ვიყენებთ checked-ის საშუალებით 
-  // თუ არ არის არცერთი მონიშნული აქაც გამოჩნდება ერორი თავისი წარწერით
 
   let checkInput = document.getElementById("agree").checked;
 
@@ -232,7 +215,6 @@ formElement.addEventListener("submit", function (event) {
     errors.check = "You must Agree Our Terms and Conditions";
   }
 
-  // დავასელექთოთ ყველა ის პე ტექსტი რომ გასუფთავდეს კონკრეტული ტექსტი დასაბმითების შემდეგ.
 
   formElement.querySelectorAll(".error-text").forEach((el) => {
     el.innerText = " ";
@@ -243,11 +225,11 @@ formElement.addEventListener("submit", function (event) {
   for (let item in errors) {
     console.log(item); // check; gender; passw;username
 
-    // errorText -ცვლადი, დავასელექთოთ ყველა შეცდომის პე - აიდის საშუალებით, ყველას აქვს ერთნაირი პრინციპი, ყველას აწერია ერორ და მერე ნეიმ ატრიბუტი, ანუ ვწერთ "error-" + item
+  
 
     let errorText = document.getElementById("error-" + item);
 
-    // თუ მოხდა შეცდომა, და გვინდა შესაბამისი ტექსტის გაწერა ამისთვის ვწერთ 
+  
 
     if (errorText) {
       errorText.textContent = errors[item];
@@ -289,11 +271,7 @@ showHidePassword = () => {
 };
 
 icon.addEventListener("click", showHidePassword);
-// function showHidePassword() {
 
-
-
-// email validation - regex
 
 let email = document.getElementById("emailfield");
 
